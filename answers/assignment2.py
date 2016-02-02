33c03d711e521f2d8c20c7da5d178d74c5f48b41
Yuankai Dong <ydong6@lsu.edu>
2016-02-02 00:58:59 -0600

    assignment 5

---
+++ answers/assignment2.py
@@ -0,0 +1,13 @@
+'''
+Created on Feb 1, 2016
+
+@author: Yuankai Dong
+'''
+
+
+def main():
+    a=list(range(2,10,2))
+    print (a,":""who do we appreciated")
+if __name__ == '__main__':
+   main()
+  
